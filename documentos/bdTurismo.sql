f79123854c8e8a67f18ecf8ab7e66086d5e08ea4
juan55860 <juan55860@gmail.com>
2014-09-27 13:40:02 -0500

    Correcion BD

--- documentos/bdTurismo.sql
+++ documentos/bdTurismo.sql
@@ -2,28 +2,26 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `mydb` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`empresas`
+-- Table `empresas`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`empresas` (
-  `id_empresas` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `empresas` (
+  `id_empresa` INT NOT NULL,
   `nombre` VARCHAR(100) NULL,
   `telefono` INT(10) NULL,
   `direccion` VARCHAR(100) NULL,
   `latitud` VARCHAR(30) NULL,
   `longitud` VARCHAR(30) NULL,
   `correo` VARCHAR(45) NULL,
-  PRIMARY KEY (`id_empresas`))
+  PRIMARY KEY (`id_empresa`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`lugar_evento`
+-- Table `lugar_evento`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`lugar_evento` (
+CREATE TABLE IF NOT EXISTS `lugar_evento` (
   `id_lugar` INT NOT NULL,
   `nombre` VARCHAR(45) NULL,
   `direccion` VARCHAR(45) NULL,
@@ -35,9 +33,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`evento`
+-- Table `evento`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`evento` (
+CREATE TABLE IF NOT EXISTS `evento` (
   `id_evento` INT NOT NULL,
   `nombre` VARCHAR(45) NULL,
   `descripcion` VARCHAR(45) NULL,
@@ -52,51 +50,49 @@ CREATE TABLE IF NOT EXISTS `mydb`.`evento` (
   INDEX `fk_evento_empresas1_idx` (`id_empresa` ASC),
   CONSTRAINT `fk_evento_lugar_evento1`
     FOREIGN KEY (`id_lugar`)
-    REFERENCES `mydb`.`lugar_evento` (`id_lugar`)
+    REFERENCES `lugar_evento` (`id_lugar`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_evento_empresas1`
     FOREIGN KEY (`id_empresa`)
-    REFERENCES `mydb`.`empresas` (`id_empresas`)
+    REFERENCES `empresas` (`id_empresa`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`publicidad`
+-- Table `publicidad`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`publicidad` (
+CREATE TABLE IF NOT EXISTS `publicidad` (
   `id_publicidad` INT NOT NULL,
   `titulo` VARCHAR(45) NULL,
   `descripcion` VARCHAR(45) NULL,
   `imagen` VARCHAR(45) NULL,
   `codigo_qr` VARCHAR(45) NULL,
   `estado` VARCHAR(45) NULL,
-  `id_empresa` VARCHAR(45) NULL,
-  `id_evento` VARCHAR(45) NULL,
   `id_empresa` INT NOT NULL,
   `id_evento` INT NOT NULL,
-  PRIMARY KEY (`id_publicidad`),
   INDEX `fk_publicidad_empresas1_idx` (`id_empresa` ASC),
   INDEX `fk_publicidad_evento1_idx` (`id_evento` ASC),
+  PRIMARY KEY (`id_publicidad`),
   CONSTRAINT `fk_publicidad_empresas1`
     FOREIGN KEY (`id_empresa`)
-    REFERENCES `mydb`.`empresas` (`id_empresas`)
+    REFERENCES `empresas` (`id_empresa`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_publicidad_evento1`
     FOREIGN KEY (`id_evento`)
-    REFERENCES `mydb`.`evento` (`id_evento`)
+    REFERENCES `evento` (`id_evento`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`tipo_publicidad`
+-- Table `tipo_publicidad`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`tipo_publicidad` (
+CREATE TABLE IF NOT EXISTS `tipo_publicidad` (
   `id_tipo` INT NOT NULL,
   `nombre` VARCHAR(45) NULL,
   `descripcion` VARCHAR(45) NULL,
@@ -105,33 +101,32 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`tipopublicidad_publicidad`
+-- Table `tipopublicidad_publicidad`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`tipopublicidad_publicidad` (
+CREATE TABLE IF NOT EXISTS `tipopublicidad_publicidad` (
   `id` INT NOT NULL,
-  `Id_TipoPublicidad` INT NOT NULL,
-  `Id_Publicidad` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_tipopublicidad_publicidad_tipo_publicidad_idx` (`Id_TipoPublicidad` ASC),
-  INDEX `fk_tipopublicidad_publicidad_publicidad1_idx` (`Id_Publicidad` ASC),
-  UNIQUE INDEX `Id_TipoPublicidad_UNIQUE` (`Id_TipoPublicidad` ASC),
+  `id_tipo_publicidad` INT NOT NULL,
+  `id_publicidad` INT NOT NULL,
+  INDEX `fk_tipopublicidad_publicidad_tipo_publicidad_idx` (`id_tipo_publicidad` ASC),
+  INDEX `fk_tipopublicidad_publicidad_publicidad1_idx` (`id_publicidad` ASC),
+  UNIQUE INDEX `Id_TipoPublicidad_UNIQUE` (`id_tipo_publicidad` ASC),
   CONSTRAINT `fk_tipopublicidad_publicidad_tipo_publicidad`
-    FOREIGN KEY (`Id_TipoPublicidad`)
-    REFERENCES `mydb`.`tipo_publicidad` (`id_tipo`)
+    FOREIGN KEY (`id_tipo_publicidad`)
+    REFERENCES `tipo_publicidad` (`id_tipo`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_tipopublicidad_publicidad_publicidad1`
-    FOREIGN KEY (`Id_Publicidad`)
-    REFERENCES `mydb`.`publicidad` (`id_publicidad`)
+    FOREIGN KEY (`id_publicidad`)
+    REFERENCES `publicidad` (`id_publicidad`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`categoria`
+-- Table `categoria`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`categoria` (
+CREATE TABLE IF NOT EXISTS `categoria` (
   `id_categoria` INT NOT NULL,
   `nombre` VARCHAR(45) NULL,
   `descripcion` VARCHAR(45) NULL,
@@ -140,9 +135,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`categoria_publicidad`
+-- Table `categoria_publicidad`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`categoria_publicidad` (
+CREATE TABLE IF NOT EXISTS `categoria_publicidad` (
   `id` INT NOT NULL,
   `id_categoria` INT NOT NULL,
   `id_publicidad` INT NOT NULL,
@@ -151,12 +146,12 @@ CREATE TABLE IF NOT EXISTS `mydb`.`categoria_publicidad` (
   INDEX `fk_categoria_publicidad_publicidad1_idx` (`id_publicidad` ASC),
   CONSTRAINT `fk_categoria_publicidad_categoria1`
     FOREIGN KEY (`id_categoria`)
-    REFERENCES `mydb`.`categoria` (`id_categoria`)
+    REFERENCES `categoria` (`id_categoria`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_categoria_publicidad_publicidad1`
     FOREIGN KEY (`id_publicidad`)
-    REFERENCES `mydb`.`publicidad` (`id_publicidad`)
+    REFERENCES `publicidad` (`id_publicidad`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
